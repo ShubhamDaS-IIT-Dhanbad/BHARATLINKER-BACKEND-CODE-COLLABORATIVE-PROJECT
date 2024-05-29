e9af6fc650444c8beb84cbb8b455c339dad74e1d
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-05-29 19:06:43 +0530

    first

--- src/api/middleware/multerMiddleware.js
+++ src/api/middleware/multerMiddleware.js
@@ -1,15 +1,21 @@
+import multer from 'multer';
+import fs from 'fs-extra';
+import path from 'path';
 
-import multer from "multer";
+// Define the directory for file uploads using __dirname
+const tempDir = path.join(/public/temp, '../../public/temp');
+
+// Ensure the directory exists
+fs.ensureDirSync(tempDir);
+
+// Configure Multer storage
 const storage = multer.diskStorage({
-  
     destination: function (req, file, cb) {
-        cb(null, "./public/temp")
+        cb(null, tempDir);
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname)
+        cb(null, file.originalname);
     }
 });
-export const upload = multer({ 
-    storage
-});
 
+export const upload = multer({ storage });
