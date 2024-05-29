aa983ba302132ae6f6b787b5e7fc46defeb60567
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-05-29 23:29:20 +0530

    firstup

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ const corsOptions = {
     }
   },
 };
-app.use(cors({
-  origin:"https://bharatlinker.shop",
-}));
+app.use(cors(corsOptions));
 
 // Logging middleware
 app.use(morgan('combined'));
