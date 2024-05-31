acb3c0b2bd8c75a220f77d252d29c0ac93bf6b85
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-05-31 11:20:11 +0530

    first

--- src/api/middleware/multerMiddleware.js
+++ src/api/middleware/multerMiddleware.js
@@ -1,13 +1,15 @@
 
 import multer from "multer";
 const storage = multer.diskStorage({
+  
     destination: function (req, file, cb) {
-        cb(null, './public/temp'); // Destination folder for storing uploaded files
+        cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-        // Ensure unique filenames by adding a timestamp prefix
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname)); // Original filename with timestamp prefix
+        cb(null, file.originalname)
     }
 });
-export const upload = multer({ storage: storage });
+export const upload = multer({ 
+    storage
+});
+
